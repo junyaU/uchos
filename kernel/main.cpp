4ecd74896c53a260e41ff9a8155b29b68fcaec9d
Junya Uchizono <junya19991215@icloud.com>
2023-07-31 22:04:07 +0900

    pass frame buffer structure to the kernel

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,10 +1,15 @@
 #include <cstdint>
 
-extern "C" void KernelMain(uint64_t frame_buffer_base,
-                           uint64_t frame_buffer_size) {
-    uint8_t* frame_buffer = reinterpret_cast<uint8_t*>(frame_buffer_base);
-    for (uint64_t i = 0; i < frame_buffer_size; ++i) {
-        frame_buffer[i] = 255;
+#include "../UchLoaderPkg/frame_buffer_conf.hpp"
+
+extern "C" void KernelMain(const FrameBufferConf& frame_buffer_conf) {
+    for (uint64_t y = 0; y < frame_buffer_conf.vertical_resolution; y++) {
+        for (uint64_t x = 0; x < frame_buffer_conf.horizontal_resolution; x++) {
+            const uint64_t i = y * frame_buffer_conf.pixels_per_scan_line + x;
+            frame_buffer_conf.frame_buffer[i * 4 + 0] = 255;
+            frame_buffer_conf.frame_buffer[i * 4 + 1] = 255;
+            frame_buffer_conf.frame_buffer[i * 4 + 2] = 255;
+        }
     }
 
     while (true) __asm__("hlt");
