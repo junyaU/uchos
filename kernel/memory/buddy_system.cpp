ee7f6d08ba135eedf623486b539dc6967198afad
Junya Uchizono <junya19991215@icloud.com>
2023-08-23 01:22:17 +0900

    implement initialization of Buddy System

--- kernel/memory/buddy_system.cpp
+++ kernel/memory/buddy_system.cpp
@@ -1,6 +1,12 @@
 #include "buddy_system.hpp"
 
-namespace {
+#include <algorithm>
+#include <cmath>
+
+#include "graphics/system_logger.hpp"
+
+namespace
+{
 char memory_pool[sizeof(BuddySystem)];
 }
 
@@ -8,18 +14,47 @@ BuddySystem* buddy_system;
 
 BuddySystem::BuddySystem() {}
 
-void InitializeBuddySystem(const MemoryMap& memory_map) {
-    buddy_system = new (memory_pool) BuddySystem();
+// Registers the given memory pages and address with the buddy system.
+// Divides the memory into appropriate order blocks and adds them to the free lists.
+void BuddySystem::RegisterMemory(int num_pages, void* addr)
+{
+	if (!addr || num_pages <= 0) {
+		return;
+	}
 
-    const auto memmap_base = reinterpret_cast<uintptr_t>(memory_map.buffer);
-    const auto memmap_end = memmap_base + memory_map.map_size;
+	while (num_pages > 0) {
+		const auto order =
+				std::min(static_cast<int>(std::log2(num_pages)), kMaxOrder);
 
-    for (uintptr_t iter = memmap_base; iter < memmap_end;
-         iter += memory_map.descriptor_size) {
-        auto descriptor = reinterpret_cast<MemoryDescriptor*>(iter);
+		this->free_lists_[order].push_back(addr);
 
-        if (!IsAvailableMemory(static_cast<MemoryType>(descriptor->type))) {
-            continue;
-        }
-    }
+		auto next_addr = reinterpret_cast<uintptr_t>(addr) +
+						 kUEFIPageSize * static_cast<uintptr_t>(std::pow(2, order));
+
+		addr = reinterpret_cast<void*>(next_addr);
+		num_pages -= static_cast<int>(std::pow(2, order));
+	}
+}
+
+// Initializes the buddy system by iterating over the UEFI memory map.
+// Registers available memory regions with the buddy system.
+void InitializeBuddySystem(const MemoryMap& memory_map)
+{
+	buddy_system = new (memory_pool) BuddySystem();
+
+	const auto memmap_base = reinterpret_cast<uintptr_t>(memory_map.buffer);
+	const auto memmap_end = memmap_base + memory_map.map_size;
+
+	for (uintptr_t iter = memmap_base; iter < memmap_end;
+		 iter += memory_map.descriptor_size) {
+		auto descriptor = reinterpret_cast<MemoryDescriptor*>(iter);
+
+		if (!IsAvailableMemory(static_cast<MemoryType>(descriptor->type))) {
+			continue;
+		}
+
+		buddy_system->RegisterMemory(
+				static_cast<int>(descriptor->number_of_pages),
+				reinterpret_cast<void*>(descriptor->physical_start));
+	}
 }
--- kernel/memory/buddy_system.hpp
+++ kernel/memory/buddy_system.hpp
@@ -10,17 +10,20 @@
 
 static const size_t kPoolSize = 4 * 1024;
 
-static const auto kMaxOrder = 8;
+static const auto kMaxOrder = 10;
 
-class BuddySystem {
-   public:
-    BuddySystem();
+class BuddySystem
+{
+public:
+	BuddySystem();
 
-   private:
-    std::array<std::list<void *, PoolAllocator<void *, kPoolSize>>, kMaxOrder>
-        free_lists_;
+	void RegisterMemory(int num_pages, void* addr);
+
+private:
+	std::array<std::list<void*, PoolAllocator<void*, kPoolSize>>, kMaxOrder + 1>
+			free_lists_;
 };
 
-extern BuddySystem *buddy_system;
+extern BuddySystem* buddy_system;
 
-void InitializeBuddySystem(const MemoryMap &memory_map);
+void InitializeBuddySystem(const MemoryMap& memory_map);
