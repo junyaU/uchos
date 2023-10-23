b955ca58944f8c3f842ad9f1b3882cb6c4b7086b
Junya Uchizono <junya19991215@icloud.com>
2023-10-23 22:08:44 +0900

    Set custom compiler settings in CMakeLists

--- kernel/graphics/font.cpp
+++ kernel/graphics/font.cpp
@@ -3,21 +3,19 @@
 #include <cstdint>
 #include <new>
 
-extern const uint8_t _binary_graphics_hankaku_bin_start;
-extern const uint8_t _binary_graphics_hankaku_bin_end;
-extern const uint8_t _binary_graphics_hankaku_bin_size;
+extern const uint8_t _binary_hankaku_bin_start;
+extern const uint8_t _binary_hankaku_bin_end;
+extern const uint8_t _binary_hankaku_bin_size;
 
 BitmapFont::BitmapFont(int width, int height)
-	: font_data_{ &_binary_graphics_hankaku_bin_start },
-	  width_{ width },
-	  height_{ height }
+	: font_data_{ &_binary_hankaku_bin_start }, width_{ width }, height_{ height }
 {
 }
 
 const uint8_t* BitmapFont::GetFont(char c)
 {
 	auto index = height_ * static_cast<unsigned int>(c);
-	if (index >= reinterpret_cast<uintptr_t>(&_binary_graphics_hankaku_bin_size)) {
+	if (index >= reinterpret_cast<uintptr_t>(&_binary_hankaku_bin_size)) {
 		return nullptr;
 	}
 
