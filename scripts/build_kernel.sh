79ece843357169038c130e8d11798b1f491234f9
Junya Uchizono <junya19991215@icloud.com>
2023-10-26 20:37:55 +0900

    Add iwyu in Github Actions

--- scripts/build_kernel.sh
+++ scripts/build_kernel.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
-cmake -B $work_path/build $work_path/kernel
-make -C $work_path/build
+IWYU_PATH=$(which include-what-you-use)
+cmake -B $work_path/build $work_path/kernel -DCMAKE_CXX_INCLUDE_WHAT_YOU_USE=${IWYU_PATH}
+VERBOSE=1 cmake --build $work_path/build
 
 cp $work_path/build/UchosKernel $mount_point/kernel.elf
