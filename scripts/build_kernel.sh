3bf7d0c295be32090637df73c916fcda413c7225
Junya Uchizono <junya19991215@icloud.com>
2023-10-26 20:50:08 +0900

    Fix iwyu options

--- scripts/build_kernel.sh
+++ scripts/build_kernel.sh
@@ -2,6 +2,6 @@
 
 IWYU_PATH=$(which include-what-you-use)
 cmake -B $work_path/build $work_path/kernel -DCMAKE_CXX_INCLUDE_WHAT_YOU_USE=${IWYU_PATH}
-VERBOSE=1 cmake --build $work_path/build
+cmake --build $work_path/build
 
 cp $work_path/build/UchosKernel $mount_point/kernel.elf
