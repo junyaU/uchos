37b24878ec14dbf35b3aef74ed6515024ea30c62
Junya Uchizono <junya19991215@icloud.com>
2023-11-17 22:56:35 +0900

    Add class_driver module

---
+++ kernel/hardware/usb/class_driver/base.hpp
@@ -0,0 +1,41 @@
+/**
+ * @file usb/class_driver/base.hpp
+ *
+ * Base class for USB class drivers.
+ * This class provides the interface and common functionalities for
+ * different types of USB class drivers. It's designed to be subclassed
+ * for specific device protocols.
+ *
+ */
+
+#pragma once
+
+#include "../endpoint.hpp"
+#include "../setup_stage_data.hpp"
+
+namespace usb
+{
+class device;
+
+class class_driver
+{
+public:
+	class_driver(device* dev);
+	virtual ~class_driver();
+
+	virtual void initialize() = 0;
+	virtual void set_endpoint(const endpoint_config& config) = 0;
+	virtual void on_endpoints_configured() = 0;
+	virtual void on_control_completed(endpoint_id ep_id,
+									  setup_stage_data* setup_data,
+									  void* buf,
+									  int len) = 0;
+	virtual void on_interrupt_completed(endpoint_id ep_id, void* buf, int len) = 0;
+
+	device* parent_device() { return device_; }
+
+private:
+	device* device_;
+};
+
+} // namespace usb
--- kernel/hardware/usb/xhci/trb.hpp
+++ kernel/hardware/usb/xhci/trb.hpp
@@ -17,7 +17,45 @@ union trb {
 		uint32_t trb_type : 6;
 		uint32_t control : 16;
 	} __attribute__((packed)) bits;
-} __attribute__((packed));
+};
+
+union setup_stage_trb {
+	static const unsigned int TYPE = 2;
+	static const unsigned int NO_DATA_STAGE = 0;
+	static const unsigned int OUT_DATA_STAGE = 2;
+	static const unsigned int IN_DATA_STAGE = 3;
+
+	std::array<uint32_t, 4> data{};
+
+	struct {
+		uint32_t request_type : 8;
+		uint32_t request : 8;
+		uint32_t value : 16;
+
+		uint32_t index : 16;
+		uint32_t length : 16;
+
+		uint32_t trb_transfer_length : 17;
+		uint32_t : 5;
+		uint32_t interrupter_target : 10;
+
+		uint32_t cycle_bit : 1;
+		uint32_t : 4;
+		uint32_t interrupt_on_completion : 1;
+		uint32_t immediate_data : 1;
+		uint32_t : 3;
+		uint32_t trb_type : 6;
+		uint32_t transfer_type : 2;
+		uint32_t : 14;
+	} __attribute__((packed)) bits;
+
+	setup_stage_trb()
+	{
+		bits.trb_type = TYPE;
+		bits.immediate_data = true;
+		bits.trb_transfer_length = 8;
+	}
+};
 
 union link_trb {
 	static const unsigned int TYPE = 6;
