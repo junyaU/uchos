622af5b3c318c531b3a2e8cba18f105f19fce87a
Junya Uchizono <junya19991215@icloud.com>
2023-12-16 12:34:44 +0900

    Add file system classes and integrate into build system
    Added new empty 'fat' class in file_system/fat.hpp and its implementation file file_system/fat.cpp, to represent FAT32 file system. Also, CMakeLists.txt has been updated in file_system/ and in the root directory, for integrating these new files into the build process. Aimed to set the foundation for FAT32 file system management.

---
+++ kernel/file_system/fat.hpp
@@ -0,0 +1,17 @@
+/*
+ * @file file_system/fat.hpp
+ *
+ * @brief FAT32 File System
+ *
+ * This file contains the definition of the `fat` class, which is designed
+ * to interact with FAT32 file systems. The FAT32 file system is a widely used
+ * file system format for removable drives and digital storage media. This class
+ * provides functionalities for reading, writing, and managing files and directories
+ * in a FAT32 file system.
+ *
+ */
+#pragma once
+
+class fat
+{
+};
