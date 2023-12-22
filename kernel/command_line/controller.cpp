0517456f3a0298e3b65d1413b0d26bd5d5dc456f
Junya Uchizono <junya19991215@icloud.com>
2023-12-22 10:42:31 +0900

    Add ELF support and argument parsing to file execution
    Updated file execution in FAT file system to support Executable and Linkable Format (ELF). Added argument parsing functionality to the execute_file function. This update allows passing of arguments to the executed file and execution of ELF formatted files. Also enhanced command handling and added 'cat' command in `controller.cpp`.

--- kernel/command_line/controller.cpp
+++ kernel/command_line/controller.cpp
@@ -14,19 +14,34 @@ void controller::process_command(const char* command, terminal& term)
 	history_write_index_ == MAX_HISTORY - 1 ? history_write_index_
 											: history_write_index_++;
 
-	if (strcmp(command, "ls") == 0) {
-		ls(term, "/");
-	} else if (strcmp(command, "cat") == 0) {
-		cat(term, "KERNEL.ELF");
-	} else {
-		auto* entry = file_system::find_directory_entry(command, 0);
-		if (entry != nullptr) {
-			file_system::execute_file(*entry);
-			return;
-		}
-
-		term.printf("Command not found: %s", command);
+	const char* args = strchr(command, ' ');
+	int command_length = strlen(command);
+	if (args != nullptr) {
+		command_length = args - command;
+		++args;
 	}
+	char command_name[command_length + 1];
+	memcpy(command_name, command, command_length);
+	command_name[command_length] = '\0';
+
+	if (strcmp(command_name, "ls") == 0) {
+		ls(term, "/");
+		return;
+	}
+
+	if (strcmp(command_name, "cat") == 0) {
+		cat(term, "KERNEL.ELF");
+		return;
+	}
+
+	auto* entry = file_system::find_directory_entry(command_name, 0);
+	if (entry != nullptr) {
+		const int status = file_system::execute_file(*entry, args);
+		term.printf("Command exited with status %d", status);
+		return;
+	}
+
+	term.printf("Command not found: %s", command_name);
 }
 
 } // namespace command_line
---
+++ kernel/file_system/elf.hpp
@@ -0,0 +1,75 @@
+#pragma once
+
+#include <cstdint>
+
+typedef uintptr_t elf64_addr_t;
+typedef uint64_t elf64_off_t;
+typedef uint16_t elf64_half_t;
+typedef uint32_t elf64_word_t;
+typedef int32_t elf64_sword_t;
+typedef uint64_t elf64_xword_t;
+typedef int64_t elf64_sxword_t;
+
+#define EI_NIDENT 16
+
+typedef struct {
+	unsigned char e_ident[EI_NIDENT];
+	elf64_half_t e_type;
+	elf64_half_t e_machine;
+	elf64_word_t e_version;
+	elf64_addr_t e_entry;
+	elf64_off_t e_phoff;
+	elf64_off_t e_shoff;
+	elf64_word_t e_flags;
+	elf64_half_t e_ehsize;
+	elf64_half_t e_phentsize;
+	elf64_half_t e_phnum;
+	elf64_half_t e_shentsize;
+	elf64_half_t e_shnum;
+	elf64_half_t e_shstrndx;
+} elf64_ehdr_t;
+
+typedef struct {
+	elf64_word_t p_type;
+	elf64_word_t p_flags;
+	elf64_off_t p_offset;
+	elf64_addr_t p_vaddr;
+	elf64_addr_t p_paddr;
+	elf64_xword_t p_filesz;
+	elf64_xword_t p_memsz;
+	elf64_xword_t p_align;
+} elf64_phdr_t;
+
+#define PT_NULL 0
+#define PT_LOAD 1
+#define PT_DYNAMIC 2
+#define PT_INTERP 3
+#define PT_NOTE 4
+#define PT_SHLIB 5
+#define PT_PHDR 6
+#define PT_TLS 7
+
+typedef struct {
+	elf64_sxword_t d_tag;
+	union {
+		elf64_xword_t d_val;
+		elf64_addr_t d_ptr;
+	} d_un;
+} elf64_dyn_t;
+
+#define DT_NULL 0
+#define DT_RELA 7
+#define DT_RELASZ 8
+#define DT_RELAENT 9
+
+typedef struct {
+	elf64_addr_t r_offset;
+	elf64_xword_t r_info;
+	elf64_sxword_t r_addend;
+} elf64_rela_t;
+
+#define ELF64_R_SYM(i) ((i) >> 32)
+#define ELF64_R_TYPE(i) ((i) & 0xffffffffL)
+#define ELF64_R_INFO(s, t) (((s) << 32) + ((t) & 0xffffffffL))
+
+#define R_X86_64_RELATIVE 8
