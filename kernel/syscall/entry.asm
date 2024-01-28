bd540ea87b87a1ec607a2d27829546ec1a958998
Junya Uchizono <junya19991215@icloud.com>
2024-01-28 20:59:02 +0900

    Add convention comments to assembly files
    This commit adds comments describing the System V AMD64 Calling Convention, including caller-saved and callee-saved registers, to multiple assembly files. This clarifies which registers are used and preserved across function calls, aiding in understanding and debugging of the assembly code.

--- kernel/syscall/entry.asm
+++ kernel/syscall/entry.asm
@@ -1,5 +1,7 @@
 ; System V AMD64 Calling Convention
 ; Registers(args): RDI, RSI, RDX, RCX, R8, R9
+; Caller-saved registers: RAX, RDI, RSI, RDX, RCX, R8, R9, R10, R11
+; Callee-saved registers: RBX, RBP, R12, R13, R14, R15
 
 bits 64
 section .text
