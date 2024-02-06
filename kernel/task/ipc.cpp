d7aceac83981497eea748f896cb70aeb1252f2b7
Junya Uchizono <junya19991215@icloud.com>
2024-02-06 20:06:31 +0900

    Add ipc handling and error definitions for tasks
    Implemented the Inter-Process Communication (IPC) system for task handling, together with error definitions related to invalid tasks and arguments in the types file. New IPC related files, 'ipc.hpp' and 'ipc.cpp', were also included in the build system. The IPC system allows tasks to send messages to each other, which enhances the communications between different tasks.

---
+++ kernel/task/ipc.cpp
@@ -0,0 +1,22 @@
+#include "ipc.hpp"
+#include "../graphics/terminal.hpp"
+#include "../types.hpp"
+#include "task.hpp"
+
+error_t send_message(task_t dst_id, const message* m)
+{
+	if (dst_id == -1 || dst_id == CURRENT_TASK->id) {
+		main_terminal->errorf("invalid destination task id: %d", dst_id);
+		return ERR_INVALID_ARG;
+	}
+
+	task* dst = tasks[dst_id];
+	if (dst == nullptr) {
+		main_terminal->errorf("task %d is not found", dst_id);
+		return ERR_INVALID_TASK;
+	}
+	
+	dst->messages.push(*m);
+
+	return OK;
+}
