74ae5119345070a2539f6351bb1529bdca76330a
junyaU <junya19991215@icloud.com>
2024-03-15 11:58:56 +0900

    Refactor print functions and adjust coordinates

--- libs/user/print.hpp
+++ libs/user/print.hpp
@@ -2,6 +2,9 @@
 
 #include <cstdint>
 
+constexpr int FONT_WIDTH = 8;
+constexpr int FONT_HEIGHT = 16;
+
 void print_text(int x, int y, const char* text, uint32_t color);
 
 void delete_char(int x, int y);
