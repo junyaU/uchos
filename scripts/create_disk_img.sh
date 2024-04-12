e5cba12dfae21118d4ce1c5f7debeac53f6154ea
junyaU <junya19991215@icloud.com>
2024-04-13 02:15:03 +0900

    Add support for storage disk image in run_qemu.sh

--- scripts/create_disk_img.sh
+++ scripts/create_disk_img.sh
@@ -4,8 +4,15 @@ if [ -e $disk_img ]; then
     rm $disk_img
 fi
 
+if [ -e $storage_img ]; then
+    rm $storage_img
+fi
+
 sudo mkdir -p $mount_point
 
 qemu-img create -f raw $disk_img 200M
 mkfs.fat -n 'UCH OS' -s 2 -f 2 -R 32 -F 32 $disk_img
 sudo mount -o loop $disk_img $mount_point
+
+# Create a storage disk image
+qemu-img create -f raw $storage_img 1G
