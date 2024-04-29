5e00f12ba4724030cf1258781d6f9581db7e977f
junyaU <junya19991215@icloud.com>
2024-04-29 22:38:31 +0900

    Add interrupt handler for Virtio PCI devices

--- kernel/interrupt/handlers.hpp
+++ kernel/interrupt/handlers.hpp
@@ -20,4 +20,6 @@ struct interrupt_frame {
 
 void on_xhci_interrupt(interrupt_frame* frame);
 
+void on_virtio_interrupt(interrupt_frame* frame);
+
 [[gnu::no_caller_saved_registers]] void kill_userland(interrupt_frame* frame);
