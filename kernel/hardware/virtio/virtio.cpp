787097bb6f673910f025ad563b4759dc54b2c2ec
junyaU <junyaadgj@gmail.com>
2024-05-02 01:52:13 +0900

    Add support for handling different types of Virtio PCI capabilities

--- kernel/hardware/virtio/virtio.cpp
+++ kernel/hardware/virtio/virtio.cpp
@@ -28,9 +28,29 @@ error_t init_virtio_pci()
 			pci::msi_delivery_mode::FIXED, interrupt_vector::VIRTIO, 0);
 
 	virtio_pci_cap* caps = nullptr;
-	size_t cap_entry = find_virtio_pci_cap(*virtio_dev, &caps);
+	find_virtio_pci_cap(*virtio_dev, &caps);
+
 	while (caps != nullptr) {
-		printk(KERN_ERROR, "Virtio PCI cap: %d", caps->first_dword.fields.cfg_type);
+		switch (caps->first_dword.fields.cfg_type) {
+			case VIRTIO_PCI_CAP_COMMON_CFG:
+				printk(KERN_ERROR, "found VIRTIO_PCI_CAP_COMMON_CFG");
+				break;
+			case VIRTIO_PCI_CAP_NOTIFY_CFG:
+				printk(KERN_ERROR, "found VIRTIO_PCI_CAP_NOTIFY_CFG");
+				break;
+			case VIRTIO_PCI_CAP_ISR_CFG:
+				printk(KERN_ERROR, "found VIRTIO_PCI_CAP_ISR_CFG");
+				break;
+			case VIRTIO_PCI_CAP_DEVICE_CFG:
+				printk(KERN_ERROR, "found VIRTIO_PCI_CAP_DEVICE_CFG");
+				break;
+			case VIRTIO_PCI_CAP_PCI_CFG:
+				printk(KERN_ERROR, "found VIRTIO_PCI_CAP_PCI_CFG");
+				break;
+			default:
+				printk(KERN_ERROR, "Unknown virtio pci cap");
+		}
+
 		caps = caps->next;
 	}
 
