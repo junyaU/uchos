237061d686eae62aa08096a128fc89fa17416696
junyaU <junyaadgj@gmail.com>
2024-05-03 01:23:43 +0900

    Add virtio-blk feature flags

---
+++ kernel/hardware/virtio/blk.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+constexpr int VIRTIO_BLK_F_SIZE_MAX = 1;
+constexpr int VIRTIO_BLK_F_SEG_MAX = 2;
+constexpr int VIRTIO_BLK_F_GEOMETRY = 4;
+constexpr int VIRTIO_BLK_F_RO = 5;
+constexpr int VIRTIO_BLK_F_BLK_SIZE = 6;
+constexpr int VIRTIO_BLK_F_FLUSH = 9;
+constexpr int VIRTIO_BLK_F_TOPOLOGY = 10;
+constexpr int VIRTIO_BLK_F_CONFIG_WCE = 11;
+constexpr int VIRTIO_BLK_F_DISCARD = 13;
+constexpr int VIRTIO_BLK_F_WRITE_ZEROES = 14;
+constexpr int VIRTIO_BLK_F_LIFE_TIME = 15;
+constexpr int VIRTIO_BLK_F_SECURE_ERASE = 16;
+constexpr int VIRTIO_BLK_F_ZONED = 17;
