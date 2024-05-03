6fe5ac01241ba5ab16c6bfd34b35c566421a39a1
junyaU <junyaadgj@gmail.com>
2024-05-03 22:55:58 +0900

    Add Virtio Device Features

--- kernel/hardware/virtio/virtio.hpp
+++ kernel/hardware/virtio/virtio.hpp
@@ -10,4 +10,17 @@ constexpr int VIRTIO_STATUS_FEATURES_OK = 8;
 constexpr int VIRTIO_STATUS_DEVICE_NEEDS_RESET = 64;
 constexpr int VIRTIO_STATUS_FAILED = 128;
 
+// Virtio Device Features
+constexpr int VIRTIO_F_INDIRECT_DESC = 28;
+constexpr int VIRTIO_F_EVENT_IDX = 29;
+constexpr int VIRTIO_F_VERSION_1 = 32;
+constexpr int VIRTIO_F_ACCESS_PLATFORM = 33;
+constexpr int VIRTIO_F_RING_PACKED = 34;
+constexpr int VIRTIO_F_IN_ORDER = 35;
+constexpr int VIRTIO_F_ORDER_PLATFORM = 36;
+constexpr int VIRTIO_F_SR_IOV = 37;
+constexpr int VIRTIO_F_NOTIFICATION_DATA = 38;
+constexpr int VIRTIO_F_NOTIF_CONFIG_DATA = 39;
+constexpr int VIRTIO_F_RING_RESET = 40;
+
 error_t init_virtio_pci();
