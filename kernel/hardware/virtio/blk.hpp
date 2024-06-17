693826738f0e28caf5f4a0993b4bd5921b151e14
junyaU <junya19991215@icloud.com>
2024-06-17 23:47:34 +0900

    refactor: Update virtio_pci_device negotiation and setup functions

--- kernel/hardware/virtio/blk.hpp
+++ kernel/hardware/virtio/blk.hpp
@@ -50,9 +50,6 @@ struct virtio_blk_req {
 
 error_t write_to_blk_device(void* buffer, uint64_t sector, uint32_t len);
 
-error_t read_from_blk_device(void* buffer,
-							 uint64_t sector,
-							 uint32_t len,
-							 virtio_pci_device* dev);
+error_t read_from_blk_device(void* buffer, uint64_t sector, uint32_t len);
 
 error_t init_blk_device();
