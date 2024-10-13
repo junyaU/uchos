805054e3d150204caf98de3c1ce920d0459c70dd
junyaU <junya19991215@icloud.com>
2024-10-13 16:54:24 +0900

    Refactor: Add wait_for_message function for userland

--- userland/commands/free/main.cpp
+++ userland/commands/free/main.cpp
@@ -10,26 +10,13 @@ extern "C" int main(int argc, char** argv)
 	pid_t pid = sys_getpid();
 
 	message m = { .type = msg_t::IPC_MEMORY_USAGE, .sender = pid };
-
 	send_message(KERNEL_TASK_ID, &m);
 
-	message msg;
-	while (true) {
-		receive_message(&msg);
-		if (msg.type == msg_t::NO_TASK) {
-			continue;
-		}
+	message msg = wait_for_message(msg_t::IPC_MEMORY_USAGE);
 
-		if (msg.type != msg_t::IPC_MEMORY_USAGE) {
-			continue;
-		}
-
-		printu("Used memory: %u MiB\nTotal memory: %u MiB",
-			   msg.data.memory_usage.used / 1024 / 1024,
-			   msg.data.memory_usage.total / 1024 / 1024);
-
-		exit(0);
-	}
+	printu("Used memory: %u MiB\nTotal memory: %u MiB",
+		   msg.data.memory_usage.used / 1024 / 1024,
+		   msg.data.memory_usage.total / 1024 / 1024);
 
 	exit(0);
 }
