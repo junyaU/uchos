880d222f6c1cb43099ae7a2de07abc6bdf08603b
junyaU <junya19991215@icloud.com>
2024-10-28 21:03:00 +0900

    Refactor: Rename call_userland to enter_user_mode

--- kernel/asm_utils.h
+++ kernel/asm_utils.h
@@ -11,12 +11,12 @@ int most_significant_bit(uint32_t value);
 
 void write_msr(uint32_t msr, uint64_t value);
 
-void call_userland(int argc,
-				   char** argv,
-				   uint16_t ss,
-				   uint64_t rip,
-				   uint64_t rsp,
-				   uint64_t* kernel_rsp);
+void enter_user_mode(int argc,
+					 char** argv,
+					 uint16_t ss,
+					 uint64_t rip,
+					 uint64_t rsp,
+					 uint64_t* kernel_rsp);
 
 void exit_userland(uint64_t kernel_rsp, int32_t status);
 }
