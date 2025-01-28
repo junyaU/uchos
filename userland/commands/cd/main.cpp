3ddb341253ed53dcdb32e7ee70185e7746d55499
junyaU <junya19991215@icloud.com>
2025-01-28 20:13:26 +0900

    Feat: Add bootstrap allocator tests and implement basic command functionality

---
+++ userland/commands/cd/main.cpp
@@ -0,0 +1,14 @@
+#include <cstring>
+#include <libs/common/types.hpp>
+#include <libs/user/console.hpp>
+#include <libs/user/ipc.hpp>
+#include <libs/user/syscall.hpp>
+
+int main(int argc, char** argv)
+{
+	char* input = argv[1];
+	if (input == nullptr) {
+	}
+
+	return 0;
+}
