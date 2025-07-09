d9ca1dabffe22dd41163abc190c19e7f95240ac8
junyaU <junya19991215@icloud.com>
2025-07-09 20:49:43 +0900

    refactor: clean up includes and simplify printu

--- libs/user/console.cpp
+++ libs/user/console.cpp
@@ -1,11 +1,8 @@
 #include <cstdarg>
 #include <cstdio>
 #include <cstring>
-#include <libs/common/message.hpp>
 #include <libs/user/console.hpp>
-#include <libs/user/ipc.hpp>
-#include <libs/user/syscall.hpp>
-#include <libs/common/process_id.hpp>
+#include <unistd.h>
 
 void printu(const char* format, ...)
 {
@@ -15,12 +12,5 @@ void printu(const char* format, ...)
 	vsprintf(buffer, format, args);
 	va_end(args);
 
-	ProcessId pid = ProcessId::from_raw(sys_getpid());
-
-	message m = { .type = msg_t::NOTIFY_WRITE,
-				  .sender = pid,
-				  .is_end_of_message = true };
-	memcpy(m.data.write_shell.buf, buffer, sizeof(buffer));
-
-	send_message(process_ids::SHELL, &m);
+	write(STDOUT_FILENO, buffer, strlen(buffer));
 }
