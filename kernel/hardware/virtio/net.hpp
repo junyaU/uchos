f95b6f513d97cf1e0df9261f882d76aec3d9d4a8
junyaU <junya19991215@icloud.com>
2025-10-25 13:33:54 +0900

    feat: Add VirtIO network feature negotiation

--- kernel/hardware/virtio/net.hpp
+++ kernel/hardware/virtio/net.hpp
@@ -10,7 +10,22 @@
  */
 #pragma once
 
+#include <cstdint>
+
 namespace kernel::hw::virtio
 {
+constexpr uint64_t VIRTIO_NET_F_CSUM = (1ULL << 0);
+constexpr uint64_t VIRTIO_NET_F_GUEST_CSUM = (1ULL << 1);
+constexpr uint64_t VIRTIO_NET_F_MAC = (1ULL << 5);
+constexpr uint64_t VIRTIO_NET_F_GSO = (1ULL << 6);
+constexpr uint64_t VIRTIO_NET_F_GUEST_TSO4 = (1ULL << 7);
+constexpr uint64_t VIRTIO_NET_F_GUEST_TSO6 = (1ULL << 8);
+constexpr uint64_t VIRTIO_NET_F_HOST_TSO4 = (1ULL << 11);
+constexpr uint64_t VIRTIO_NET_F_HOST_TSO6 = (1ULL << 12);
+constexpr uint64_t VIRTIO_NET_F_MRG_RXBUF = (1ULL << 15);
+constexpr uint64_t VIRTIO_NET_F_STATUS = (1ULL << 16);
+constexpr uint64_t VIRTIO_NET_F_CTRL_VQ = (1ULL << 17);
+constexpr uint64_t VIRTIO_NET_F_MQ = (1ULL << 22);
+
 void virtio_net_service();
 } // namespace kernel::hw::virtio
